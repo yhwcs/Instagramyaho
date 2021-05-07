9da3320fce4f99687dcfa04f1b992ce1419c7415
Jang Seowoo <serajang1679@gmail.com>
2021-05-07 10:44:03 +0900

    Initial Commit

---
+++ Instagramyaho/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Instagramyaho
+//
+//  Created by 장서우 on 2021/05/07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
